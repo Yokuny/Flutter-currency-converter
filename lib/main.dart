cf8540d5a908ae275d9bd43c155b06c17e6b49cf
yokuny <felipe.vni@hotmail.com>
2020-10-12 10:07:22 -0300

    an error correction

--- lib/main.dart
+++ lib/main.dart
@@ -147,7 +147,7 @@ Widget buildTextField(String label, String prefix, TextEditingController coin,
         border: OutlineInputBorder(),
         prefixText: prefix),
     style: TextStyle(color: Colors.amber, fontSize: 25.0),
-    onChanged: currentCoin(),
+    onChanged: currentCoin,
     keyboardType: TextInputType.numberWithOptions(decimal: true),
   );
 }
